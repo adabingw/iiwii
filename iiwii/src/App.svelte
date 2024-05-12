5e6f06e06248c568a53bd352e294d3e307b8c0fd
Ada <adabingw@yahoo.com>
2024-05-12 08:50:34 -0400

    :sake:

---
+++ iiwii/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+import Header from "./header/Header.svelte";
+
+</script>
+
+<div>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+</div>
+
+<div>
+    <Header />
+</div>
+
+<style>
+
+</style>
---
+++ iiwii/src/header/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div class="w-full pt-5 h-15 ">
+    <i class="fa-regular fa-sun fa-xl"></i>
+    <i class="fa-regular fa-moon fa-xl"></i>
+</div>
+
+<style>
+
+</style>
